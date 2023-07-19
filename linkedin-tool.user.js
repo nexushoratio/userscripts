c9ba4dbc64b781aa2191b31d51de27fa18c22779
Mike Castle <dalgoda@gmail.com>
2023-07-19 12:32:29 -0700

    Forgot to bump version number on last change.

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -2,7 +2,7 @@
 // @name        LinkedIn Tool
 // @namespace   dalgoda@gmail.com
 // @match       https://www.linkedin.com/*
-// @version     0.02.3
+// @version     0.03
 // @author      Mike Castle
 // @description Add some stuff to LinkedIn.  So far, just keystrokes.
 // @grant       GM_addStyle
