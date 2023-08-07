cf18d4b70dd28dd9e9626a0e81497e9ab262aa61
Mike Castle <dalgoda@gmail.com>
2023-08-06 20:59:00 -0700

    Bump version number.

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -2,7 +2,7 @@
 // @name        LinkedIn Tool
 // @namespace   dalgoda@gmail.com
 // @match       https://www.linkedin.com/*
-// @version     2.0.1
+// @version     2.0.2
 // @author      Mike Castle
 // @description Minor enhancements to LinkedIn. Mostly just hotkeys.
 // @license     GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
