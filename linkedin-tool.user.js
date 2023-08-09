nexushoratio/userscripts

commit 2df7434a40444616b734805db1268ac6c40a4fc5

Major refactoring in support of one set of hotkeys per view.
Mostly this was moving things into new classes.  Some clean up of
other names and selectors as well.

Since I discovered that VM.shortcuts can have multiple
KeyboardServices enabled at one time, I switched to using one of those
for global keystrokes rather than tracking them and readding them to
each service.

Some regressions:

* Clicks and TAB navigation currently do not update current focus.
  Mostly a matter of figuring out how to handle those in view of
  multiple services.

* Hotkeys do not turnoff when in an editable area (like the search
  box).

Issue #6.

linkedin-tool.user.js
@@ -20,11 +20,18 @@
   // get a working configuration.
   class Page {
     _pathname;
-    _keyboard;
+    _keyboard = new VM.shortcut.KeyboardService();
+    _auto_keys = [];
 
-    constructor(pathname) {
-      this._pathname = pathname;
-      this._keyboard = new VM.shortcut.KeyboardService();
+    static _navOption = {
+      caseSensitive: true,
+      condition: '!inputFocus',
+    };
+
+    start() {
+      for (const {key, func} of this._auto_keys) {
+	this._addKey(key, func.bind(this));
+      }
     }
 
     get pathname() {
@@ -34,34 +41,256 @@
     activate() {
       this._keyboard.enable();
     }
+
+    deactivate() {
+      this._keyboard.disable();
+    }
+
+    _addKey(key, func) {
+      this._keyboard.register(key, func, Page._navOption);
+    }
+
+  }
+
+  class Global extends Page {
+    _pathname = null;
+    _auto_keys = [
+      {key: '?', description: 'Help', func: this._help},
+      {key: '/', description: 'Search', func: this._gotoSearch},
+      {key: 'g h', description: 'Go Home (aka, Feed)', func: this._goHome},
+      {key: 'g m', description: 'Go to My Network', func: this._gotoMyNetwork},
+      {key: 'g j', description: 'Go to Jobs', func: this._gotoJobs},
+      {key: 'g g', description: 'Go to Messaging', func: this._gotoMessaging},
+      {key: 'g n', description: 'Go to Notifications', func: this._gotoNotifications},
+      {key: 'g p', description: 'Go to Profile (aka, Me)', func: this._gotoProfile},
+      {key: 'g b', description: 'Go to Business', func: this._gotoBusiness},
+      {key: 'g l', description: 'Go to Learning', func: this._gotoLearning},
+    ];
+
+    _gotoNavLink(item) {
+      document.querySelector(`#global-nav a[href*="/${item}"`).click();
+    }
+
+    _gotoNavButton(item) {
+      const buttons = document.querySelectorAll('#global-nav button');
+      for (const el of buttons) {
+	if (el.textContent.includes(item)) {
+	  el.click();
+	  break;
+	}
+      }
+    }
+
+    _help() {
+      alert('The help is in another castle.');
+    }
+
+    _gotoSearch() {
+      document.querySelector('#global-nav-search input').focus();
+    }
+
+    _goHome() {
+      this._gotoNavLink('feed');
+    }
+
+    _gotoMyNetwork() {
+      this._gotoNavLink('mynetwork');
+    }
+
+    _gotoJobs() {
+      this._gotoNavLink('jobs');
+    }
+
+    _gotoMessaging() {
+      this._gotoNavLink('messaging');
+    }
+
+    _gotoNotifications() {
+      this._gotoNavLink('notifications');
+    }
+
+    _gotoProfile() {
+      this._gotoNavButton('Me');
+    }
+
+    _gotoBusiness() {
+      this._gotoNavButton('Business');
+    }
+
+    _gotoLearning() {
+      this._gotoNavLink('learning');
+    }
+
+  }
+
+  class Feed extends Page {
+    _pathname = '/feed/';
+    _auto_keys = [
+      {key: 'j', description: 'Next post', func: this._nextPost},
+      {key: 'J', description: 'Toggle hiding then next post', func: this._nextPostPlus},
+      {key: 'k', description: 'Previous post', func: this._prevPost},
+      {key: 'K', description: 'Toggle hiding then previous post', func: this._prevPostPlus},
+      {key: 'X', description: 'Toggle hiding post', func: this._togglePost},
+      {key: 'c', description: 'Show comments', func: this._showComments},
+      {key: 'm', description: 'Show more of the post', func: this._seeMore},
+      {key: 'l', description: 'Load more posts (if [New Posts] button is available, load those)', func: this._loadMorePosts},
+      {key: 'L', description: 'Like post', func: this._likePost},
+    ];
+
+    _postIndex = -1;
+    _postId = null;
+
+    _getPosts() {
+      return document.querySelectorAll('main div[data-id]');
+    }
+
+    _scrollToPost(post) {
+      post.scrollIntoView();
+      // TODO(https://github.com/nexushoratio/userscripts/issues/9)
+      post.setAttribute('tabindex', 0);
+      post.focus();
+      this._postId = post.dataset.id;
+    }
+
+    _scrollBy(n, recursed = false) {
+      const posts = this._getPosts();
+      this._postIndex = Math.max(Math.min(this._postIndex +n, posts.length), 0);
+      const post = posts[this._postIndex];
+      // Some posts are hidden, (ads, suggestions).  Skip over at least one.
+      if (post.clientHeight === 0 && !recursed) {
+	this._scrollBy(n, true);
+      } else {
+	this._scrollToPost(post);
+      }
+    }
+
+    _nextPost() {
+      this._scrollBy(1);
+    }
+
+    _nextPostPlus() {
+      this._togglePost();
+      this._nextPost();
+    }
+
+    _prevPost() {
+      this._scrollBy(-1);
+    }
+
+    _prevPostPlus() {
+      this._togglePost();
+      this._prevPost();
+    }
+
+    _togglePost() {
+      const post = this._getPosts()[this._postIndex];
+      if (post) {
+	const dismiss = post.querySelector('button[aria-label^="Dismiss post"]');
+	if (dismiss) {
+	  dismiss.click();
+	} else {
+	  const undo = post.querySelector('button[aria-label^="Undo and show"]');
+	  if (undo) {
+	    undo.click();
+	  }
+	}
+      }
+    }
+
+    _showComments() {
+      const post = this._getPosts()[this._postIndex];
+      if (post) {
+	const comments = post.querySelector('button[aria-label*="comment"]');
+	if (comments) {
+	  comments.click();
+	}
+      }
+    }
+
+    _seeMore() {
+      const post = this._getPosts()[this._postIndex];
+      if (post) {
+	const see_more = post.querySelector('button[aria-label^="see more"]');
+	if (see_more) {
+	  see_more.click();
+	}
+      }
+    }
+
+    _likePost() {
+      const post = this._getPosts()[this._postIndex];
+      if (post) {
+	const like_button = post.querySelector('button[aria-label^="Open reactions menu"]');
+	like_button.click();
+      }
+    }
+
+    _loadMorePosts() {
+      const posts = this._getPosts();
+      const new_updates = posts[0].querySelector('div.feed-new-update-pill button');
+      if (new_updates) {
+	new_updates.click();
+	this._postIndex = -1;
+      } else {
+	const show_more = document.querySelector('main button.scaffold-finite-scroll__load-button');
+	if (show_more) {
+	  show_more.click();
+	  posts[this._postIndex].focus();
+	}
+      }
+    }
+
+  }
+
+  class Jobs extends Page {
+    _pathname = '/jobs/';
+  }
+
+  class JobsCollections extends Page {
+    _pathname = '/jobs/collections/';
   }
 
   class Pages {
-    _pages = [];
+    _global = null;
+    _page = null;
+    _pages = new Map();
 
-    constructor() {
-      this.register('/');
+    register(page) {
+      page.start();
+      if (page.pathname === null) {
+	this._global = page;
+	this._global.activate();
+      } else {
+	this._pages.set(page.pathname, page);
+      }
     }
 
-    register(pathname) {
-      const page = new Page(pathname);
-      this._pages.push(page);
+    _findPage(pathname) {
+      const pathnames = Array.from(this._pages.keys());
+      const candidates = pathnames.filter(p => pathname.startsWith(p));
+      const candidate = candidates.reduce((a, b) => {
+	  return a.length > b.length ? a : b;
+      }, '');
+      return this._pages.get(pathname) || null;
     }
 
     activate(pathname) {
-      const candidates = this._pages.filter(page => pathname.startsWith(page.pathname));
-      const page = candidates.reduce((a, b) => {
-	  return a.length > b.length ? a : b;
-	}
-      );
-      console.log(page);
+      if (this._page) {
+	this._page.deactivate();
+      }
+      const page = this._findPage(pathname);
+      this._page = page;
+      if (page) {
+	page.activate();
+      }
     }
   }
 
   const pages = new Pages();
-  pages.register('/feed/');
-  pages.register('/jobs/');
-  pages.register('/jobs/collections/');
+  pages.register(new Global());
+  pages.register(new Feed());
+  pages.register(new Jobs());
+  pages.register(new JobsCollections);
   pages.activate(window.location.pathname);
 
   const kbService = new VM.shortcut.KeyboardService();
@@ -69,175 +298,27 @@
     caseSensitive: true,
     condition: '!inputFocus',
   };
-  kbService.enable();
+  // kbService.enable();
 
-  const current = {
-    _post: -1,
-    get post() {
-      const post = document.activeElement.closest('div[data-id]');
-      if (post && post !== document.activeElement) {
-	const relatives = getRelatives();
-	const n = Array.prototype.findIndex.call(relatives, element => element === post);
-	this._post = n;
-      }
-      return this._post;
-    },
-    set post(v) {
-      this._post = v;
-    }
-  };
-
-  kbService.register('/', gotoSearch, navOption);
-  kbService.register('g h', () => {
-    gotoNavLink('feed');
-  }, navOption);
-  kbService.register('g m', () => {
-    gotoNavLink('mynetwork');
-  }, navOption);
-  kbService.register('g j', () => {
-    gotoNavLink('jobs');
-  }, navOption);
-  kbService.register('g g', () => {
-    gotoNavLink('messaging');
-  }, navOption);
-  kbService.register('g n', () => {
-    gotoNavLink('notifications');
-  }, navOption);
-  kbService.register('g p', () => {
-    gotoNavButton('Me');
-  }, navOption);
-  kbService.register('g b', () => {
-    gotoNavButton('Business');
-  }, navOption);
-  kbService.register('g l', () => {
-    gotoNavLink('learning');
-  }, navOption);
-  kbService.register('j', () => {
-    scrollBy(1);
-  }, navOption);
-  kbService.register('k', () => {
-    scrollBy(-1);
-  }, navOption);
-  kbService.register('J', () => {
-    togglePost();
-    scrollBy(1);
-  }, navOption);
-  kbService.register('K', () => {
-    togglePost();
-    scrollBy(-1);
-  }, navOption);
-  kbService.register('X', togglePost, navOption);
-  kbService.register('c', showComments, navOption);
-  kbService.register('m', seeMore, navOption);
-  kbService.register('l', loadNewPosts, navOption);
-  kbService.register('L', likeElement, navOption);
+  // TODO: Likely move into Pages.
+  // const current = {
+  //   _post: -1,
+  //   get post() {
+  //     const post = document.activeElement.closest('div[data-id]');
+  //     if (post && post !== document.activeElement) {
+  // 	const relatives = getRelatives();
+  // 	const n = Array.prototype.findIndex.call(relatives, element => element === post);
+  // 	this._post = n;
+  //     }
+  //     return this._post;
+  //   },
+  //   set post(v) {
+  //     this._post = v;
+  //   }
+  // };
 
   function isInput(element) {
     return (element.isContentEditable || element.tagName === 'INPUT');
-  }
-
-  function getRelatives() {
-    return document.querySelectorAll('main div[data-id]');
-  }
-
-  function gotoSearch() {
-    document.querySelector('#global-nav-search input').focus();
-  }
-
-  function gotoNavLink(item) {
-    const el = document.querySelector(`#global-nav a[href*="/${item}"`);
-    el.click();
-  }
-
-  function gotoNavButton(item) {
-    const buttons = document.querySelectorAll('#global-nav button');
-    for (const el of buttons) {
-      if (el.textContent.includes(item)) {
-	el.click();
-	break;
-      }
-    }
-  }
-
-  function scrollToCurrent(relatives) {
-    const el = relatives[current.post];
-    el.scrollIntoView();
-    // TODO(https://github.com/nexushoratio/userscripts/issues/9)
-    el.setAttribute('tabindex', 0);
-    el.focus();
-  }
-
-  function scrollBy(index, recursed = false) {
-    console.debug('scrolling by %d', index);
-    const relatives = getRelatives();
-    current.post = Math.max(Math.min(current.post + index, relatives.length), 0);
-    const el = relatives[current.post];
-    // Some posts are hidden.  So far, seems like just ads.
-    if (el.clientHeight === 0 && !recursed) {
-      console.debug('Skipping...', el);
-      scrollBy(index, true);
-    } else {
-      scrollToCurrent(relatives);
-    }
-  }
-
-  function togglePost() {
-    const post = getRelatives()[current.post];
-    if (post) {
-      const dismiss = post.querySelector('button[aria-label^="Dismiss post"]');
-      if (dismiss) {
-	dismiss.click();
-      } else {
-	const undo = post.querySelector('button[aria-label^="Undo and show"]');
-	if (undo) {
-	  undo.click();
-	}
-      }
-    }
-  }
-
-  function showComments() {
-    const post = getRelatives()[current.post];
-    if (post) {
-      const comments = post.querySelector('button[aria-label*="comment"]');
-      if (comments) {
-	comments.click();
-      }
-    }
-  }
-
-  function seeMore() {
-    const post = getRelatives()[current.post];
-    if (post) {
-      const see_more = post.querySelector('button[aria-label^="see more"]');
-      if (see_more) {
-	see_more.click();
-      }
-    }
-  }
-
-  function likeElement() {
-    const post = getRelatives()[current.post];
-    console.debug(post);
-    if (post) {
-      const like_button = post.querySelector('button[aria-label^="Open reactions menu"]');
-      like_button.click();
-    }
-  }
-
-  function loadNewPosts() {
-    const posts = getRelatives();
-    const first_post = posts[0].querySelector('div.feed-new-update-pill button');
-    if (first_post) {
-      first_post.click();
-      current.post = -1;
-    } else {
-      const show_more = document.querySelector('#voyager-feed button.scaffold-finite-scroll__load-button');
-      if (show_more) {
-	show_more.click();
-	posts[current.post].focus();
-      }
-    }
   }
 
   VM.observe(document.body, () => {
@@ -251,24 +332,25 @@
     }
   });
 
-  VM.observe(document.body, () => {
-    console.debug('observer for main');
-    const main = document.querySelector('main');
-    if (main) {
-      // TODO: factor this into standalone function.
-      main.addEventListener('click', (e) => {
-	const post = e.target.closest('div[data-id]');
-	if (post) {
-	  const relatives = getRelatives();
-	  const n = Array.prototype.findIndex.call(relatives, element => element === post);
-	  current.post = n;
-	  scrollToCurrent(relatives);
-	}
-      });
+  // TODO: Move this to Pages.
+  // VM.observe(document.body, () => {
+  //   console.debug('observer for main');
+  //   const main = document.querySelector('main');
+  //   if (main) {
+  //     // TODO: factor this into standalone function.
+  //     main.addEventListener('click', (e) => {
+  // 	const post = e.target.closest('div[data-id]');
+  // 	if (post) {
+  // 	  const relatives = getRelatives();
+  // 	  const n = Array.prototype.findIndex.call(relatives, element => element === post);
+  // 	  current.post = n;
+  // 	  scrollToCurrent(relatives);
+  // 	}
+  //     });
 
-      return true
-    }
-  });
+  //     return true
+  //   }
+  // });
 
   let oldUrl = new URL(window.location);
   VM.observe(document.body, () => {
@@ -280,16 +362,18 @@
     }
   });
 
-  document.addEventListener('focus', (e) => {
-    if (isInput(e.target)) {
-      kbService.setContext('inputFocus', true);
-    }
-  }, true);
-  document.addEventListener('blur', (e) => {
-    if (isInput(e.target)) {
-      kbService.setContext('inputFocus', false);
-    }
-  }, true);
+  // TODO: Most likely needs to go into Pages and inform all Page instances.
+  // document.addEventListener('focus', (e) => {
+  //   if (isInput(e.target)) {
+  //     kbService.setContext('inputFocus', true);
+  //   }
+  // }, true);
+  // document.addEventListener('blur', (e) => {
+  //   if (isInput(e.target)) {
+  //     kbService.setContext('inputFocus', false);
+  //   }
+  // }, true);
+
   document.addEventListener('href', (e) => {
     pages.activate(e.detail.new.pathname);
   }, true);

commit 33f592cf4853c3dbec40c23ddfa764402583eaad

Fix bug introduced by 2df7434a.
Found by eslint.  Missed because we do not yet support any views on
longer URLs.  Well, that and because lack of tests.

Closes #66.

linkedin-tool.user.js
@@ -2,7 +2,7 @@
 // @name        LinkedIn Tool
 // @namespace   dalgoda@gmail.com
 // @match       https://www.linkedin.com/*
-// @version     2.3.0
+// @version     2.3.1
 // @author      Mike Castle
 // @description Minor enhancements to LinkedIn. Mostly just hotkeys.
 // @license     GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
@@ -995,7 +995,7 @@
       const candidate = candidates.reduce((a, b) => {
         return a.length > b.length ? a : b;
       }, '');
-      return this._pages.get(pathname) || null;
+      return this._pages.get(candidate) || null;
     }
 
     activate(pathname) {