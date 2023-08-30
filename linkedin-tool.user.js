c7f6c76deb5b0bff54bf37e5752ac9c5aa3dbdfa
Mike Castle <dalgoda@gmail.com>
2023-08-29 19:32:57 -0700

    eslint: Enable and configure `no-bitwise` along with some fix-ups.
    Issue #68.

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -144,7 +144,7 @@
   function strHash(s) {
     let hash = 0;
     for (let i = 0; i < s.length; i++) {
-      hash = ((hash << 5) - hash) + s.charCodeAt(i) | 0;
+      hash = (hash * 31) + s.charCodeAt(i) | 0;
     }
     return `${hash}`;
   }
