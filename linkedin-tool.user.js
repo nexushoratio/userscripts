7f8636366d30f4bdc9276ece4ae6b828fd3d111b
Mike Castle <dalgoda@gmail.com>
2023-10-22 08:32:36 -0700

    Bump version numbers.
    ␄

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -3,7 +3,7 @@
 // @namespace   dalgoda@gmail.com
 // @match       https://www.linkedin.com/*
 // @noframes
-// @version     5.8
+// @version     5.9
 // @author      Mike Castle
 // @description Minor enhancements to LinkedIn. Mostly just hotkeys.
 // @license     GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0-standalone.html
@@ -4456,6 +4456,11 @@
     ];
 
     static #newsContent = [
+      {
+        date: '2023-10-22',
+        issues: ['#144'],
+        subject: 'Initial support for the *Messaging* page',
+      },
       {
         date: '2023-10-20',
         issues: ['#106'],
