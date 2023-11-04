acdbe70ec5042fb8dc55eacdcc8e91bf859b99fd
Mike Castle <dalgoda@gmail.com>
2023-11-04 11:19:29 -0700

    Bump lib/widget to version 2.
    Does not do anything yet, but it does allow me to act in developer mode with
    one less local change.
    
    Issue #185.
    
    ␄

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -14,7 +14,7 @@
 // @require     https://greasyfork.org/scripts/477290-nh-base/code/NH_base.js?version=1274745
 // @require     https://greasyfork.org/scripts/478349-nh-userscript/code/NH_userscript.js?version=1275173
 // @require     https://greasyfork.org/scripts/478440-nh-web/code/NH_web.js?version=1271884
-// @require     https://greasyfork.org/scripts/478676-nh-widget/code/NH_widget.js?version=1273298
+// @require     https://greasyfork.org/scripts/478676-nh-widget/code/NH_widget.js?version=1275172
 // @grant       window.onurlchange
 // ==/UserScript==
 
@@ -29,7 +29,7 @@
     {name: 'base', minVersion: 22},
     {name: 'userscript', minVersion: 3},
     {name: 'web', minVersion: 1},
-    {name: 'widget', minVersion: 1},
+    {name: 'widget', minVersion: 2},
   ]);
 
   // TODO(#170): Placeholder comment to allow easy patching of test code.
