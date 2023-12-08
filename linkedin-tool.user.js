a7e0eec0aaaaeb9cd5d3df165dcbb1dc6f8de38d
Mike Castle <dalgoda@gmail.com>
2023-12-08 07:33:47 -0800

    Update to most recent release of all libraries.
    We base and web some upcoming changes.
    
    Issue #205.
    
    ␄

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -10,11 +10,11 @@
 // @downloadURL https://github.com/nexushoratio/userscripts/raw/main/linkedin-tool.user.js
 // @supportURL  https://github.com/nexushoratio/userscripts/blob/main/linkedin-tool.md
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
-// @require     https://update.greasyfork.org/scripts/478188/1286498/NH_xunit.js
-// @require     https://update.greasyfork.org/scripts/477290/1285523/NH_base.js
+// @require     https://update.greasyfork.org/scripts/478188/1292418/NH_xunit.js
+// @require     https://update.greasyfork.org/scripts/477290/1292417/NH_base.js
 // @require     https://update.greasyfork.org/scripts/478349/1284417/NH_userscript.js
-// @require     https://update.greasyfork.org/scripts/478440/1276563/NH_web.js
-// @require     https://update.greasyfork.org/scripts/478676/1285522/NH_widget.js
+// @require     https://update.greasyfork.org/scripts/478440/1292415/NH_web.js
+// @require     https://update.greasyfork.org/scripts/478676/1290537/NH_widget.js
 // @grant       GM.getValue
 // @grant       GM.setValue
 // @grant       window.onurlchange
@@ -27,11 +27,11 @@
   'use strict';
 
   const NH = window.NexusHoratio.base.ensure([
-    {name: 'xunit', minVersion: 33},
-    {name: 'base', minVersion: 36},
+    {name: 'xunit', minVersion: 42},
+    {name: 'base', minVersion: 43},
     {name: 'userscript', minVersion: 5},
-    {name: 'web', minVersion: 2},
-    {name: 'widget', minVersion: 14},
+    {name: 'web', minVersion: 3},
+    {name: 'widget', minVersion: 18},
   ]);
 
   /**
