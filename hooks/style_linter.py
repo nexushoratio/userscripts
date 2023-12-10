d99131e54fd12e8cb2ffc5d3af40faa954cb76ae
Mike Castle <dalgoda@gmail.com>
2023-12-10 12:55:45 -0800

    Run through `yapf` (again).
    Settings had been tweaked (back to 4 spaces).
    
    I had tried to hold off doing this until after fixing other issues, but some
    upcoming refactoring will be easier doing this now.
    
    Issue #201.

--- hooks/style_linter.py
+++ hooks/style_linter.py
@@ -16,119 +16,119 @@ skip_re = re.compile(r'( \+= )')
 
 
 class Type(enum.IntEnum):
-  """Identify the code snippet type."""
-  NAME = 0
-  CONSTRUCTOR = enum.auto()
+    """Identify the code snippet type."""
+    NAME = 0
+    CONSTRUCTOR = enum.auto()
 
-  STATIC_PUBLIC_CLASS = enum.auto()
-  STATIC_PUBLIC_FIELD = enum.auto()
-  STATIC_PUBLIC_GETTER = enum.auto()
-  STATIC_PUBLIC_METHOD = enum.auto()
+    STATIC_PUBLIC_CLASS = enum.auto()
+    STATIC_PUBLIC_FIELD = enum.auto()
+    STATIC_PUBLIC_GETTER = enum.auto()
+    STATIC_PUBLIC_METHOD = enum.auto()
 
-  PUBLIC_FIELD = enum.auto()
-  PUBLIC_GETTER = enum.auto()
-  PUBLIC_METHOD = enum.auto()
+    PUBLIC_FIELD = enum.auto()
+    PUBLIC_GETTER = enum.auto()
+    PUBLIC_METHOD = enum.auto()
 
-  STATIC_PRIVATE_CLASS = enum.auto()
-  STATIC_PRIVATE_FIELD = enum.auto()
-  STATIC_PRIVATE_GETTER = enum.auto()
-  STATIC_PRIVATE_METHOD = enum.auto()
+    STATIC_PRIVATE_CLASS = enum.auto()
+    STATIC_PRIVATE_FIELD = enum.auto()
+    STATIC_PRIVATE_GETTER = enum.auto()
+    STATIC_PRIVATE_METHOD = enum.auto()
 
-  PRIVATE_FIELD = enum.auto()
-  PRIVATE_GETTER = enum.auto()
-  PRIVATE_METHOD = enum.auto()
+    PRIVATE_FIELD = enum.auto()
+    PRIVATE_GETTER = enum.auto()
+    PRIVATE_METHOD = enum.auto()
 
-  NESTED_TESTCASE = enum.auto()
+    NESTED_TESTCASE = enum.auto()
 
-  END = enum.auto()
+    END = enum.auto()
 
 
 @dataclasses.dataclass(order=True, frozen=True)
 class Nest:
-  """Details about the nest (often class) of an entry."""
-  indent: int
-  line: int
-  name: str
+    """Details about the nest (often class) of an entry."""
+    indent: int
+    line: int
+    name: str
 
 
 @dataclasses.dataclass(frozen=True)
 class Snippet:
-  """Snippet identifying interesting bits of code."""
-  type: Type
-  code: str
-  line: int
-  parent: Nest
+    """Snippet identifying interesting bits of code."""
+    type: Type
+    code: str
+    line: int
+    parent: Nest
 
-  def __post_init__(self):
-    pass
-    # print('post_init:', self)
+    def __post_init__(self):
+        pass
+        # print('post_init:', self)
 
-  def __lt__(self, other):
-    if self.parent == other.parent:
-      if self.type == other.type:
-        if '_FIELD' in self.type.name:
-          # Alphabetize by field name
-          return self.code < other.code
+    def __lt__(self, other):
+        if self.parent == other.parent:
+            if self.type == other.type:
+                if '_FIELD' in self.type.name:
+                    # Alphabetize by field name
+                    return self.code < other.code
 
-        if '_GETTER' in self.type.name:
-          # Parse (irony) out the name of the {g,s}etters
-          self_word = self.code.split()[-2].split('(')[0]
-          other_word = other.code.split()[-2].split('(')[0]
-          # Then alphabetize
-          return self_word < other_word
+                if '_GETTER' in self.type.name:
+                    # Parse (irony) out the name of the {g,s}etters
+                    self_word = self.code.split()[-2].split('(')[0]
+                    other_word = other.code.split()[-2].split('(')[0]
+                    # Then alphabetize
+                    return self_word < other_word
 
-        # Everything else being equal, keep current relative order
-        return self.line < other.line
+                # Everything else being equal, keep current relative order
+                return self.line < other.line
 
-      # Order by type
-      return self.type < other.type
+            # Order by type
+            return self.type < other.type
 
-    # Is it our parent?
-    if other.parent.name in self.code:
-      return False
+        # Is it our parent?
+        if other.parent.name in self.code:
+            return False
 
-    # Otherwise, order by our parents
-    return self.parent < other.parent
+        # Otherwise, order by our parents
+        return self.parent < other.parent
 
 
 def tsort(data):
-  """Perform a topological sort on the data.
+    """Perform a topological sort on the data.
 
   Mostly, place nested items under their parents.
   """
-  parents = dict()
-  tdata = collections.defaultdict(list)
+    parents = dict()
+    tdata = collections.defaultdict(list)
 
-  # Gather the parents first
-  for item in data:
-    if item.type in (Type.NAME, Type.STATIC_PUBLIC_CLASS, Type.STATIC_PRIVATE_CLASS,
-                  Type.NESTED_TESTCASE):
-      parents[item.code] = item
+    # Gather the parents first
+    for item in data:
+        if item.type in (Type.NAME, Type.STATIC_PUBLIC_CLASS,
+                         Type.STATIC_PRIVATE_CLASS, Type.NESTED_TESTCASE):
+            parents[item.code] = item
 
-  # Separate items out under their parents
-  for item in data:
-    parent = parents.get(item.parent.name)
-    tdata[parent].append(item)
+    # Separate items out under their parents
+    for item in data:
+        parent = parents.get(item.parent.name)
+        tdata[parent].append(item)
 
-  # Reassemble in order
-  results = list()
-  # Start at the root
-  working = sorted(tdata[None])
-  while working:
-    item = working.pop(0)
-    results.append(item)
+    # Reassemble in order
+    results = list()
+    # Start at the root
+    working = sorted(tdata[None])
+    while working:
+        item = working.pop(0)
+        results.append(item)
 
-    # If it has children, insert them next
-    if item in tdata:
-      old = working
-      working = sorted(tdata[item])
-      working.extend(old)
+        # If it has children, insert them next
+        if item in tdata:
+            old = working
+            working = sorted(tdata[item])
+            working.extend(old)
 
-  return results
+    return results
 
 
 def extract_snippet(code, line_number, parent, indent):
-  """Returns a Snippet() that represents the current line of code.
+    """Returns a Snippet() that represents the current line of code.
 
   Args:
     code: str, Line of code to examine.
@@ -136,144 +136,162 @@ def extract_snippet(code, line_number, parent, indent):
     parent: Nest, Parent context.
     indent: int, How many spaces the current line is indented.
   """
-  snippet = None
-  words = code.split()
-  if words[0] == 'class':
-    snippet = Snippet(Type.NAME, words[1], line_number, parent)
-  elif words[0].startswith('constructor'):
-    snippet = Snippet(Type.CONSTRUCTOR, words[0], line_number, parent)
-  elif words[0] == 'static':
-    if words[1] in ('get', 'set'):
-      if words[2][0] == '#':
-        snippet = Snippet(Type.STATIC_PRIVATE_GETTER, code, line_number, parent)
-      else:
-        snippet = Snippet(Type.STATIC_PUBLIC_GETTER, code, line_number, parent)
-    elif nested_testcase_re.search(code):
-      snippet = Snippet(Type.NESTED_TESTCASE, words[1], line_number, parent)
-    elif static_class_re.search(code):
-      if words[1][0] == '#':
-        snippet = Snippet(Type.STATIC_PRIVATE_CLASS, words[1], line_number, parent)
-      else:
-        snippet = Snippet(Type.STATIC_PUBLIC_CLASS, words[1], line_number, parent)
-    elif method_re.search(code):
-      if words[1][0] == '#':
-        snippet = Snippet(Type.STATIC_PRIVATE_METHOD, code, line_number, parent)
-      else:
-        snippet = Snippet(Type.STATIC_PUBLIC_METHOD, code, line_number, parent)
-    elif words[1][0] == '#':
-      snippet = Snippet(Type.STATIC_PRIVATE_FIELD, code, line_number, parent)
+    snippet = None
+    words = code.split()
+    if words[0] == 'class':
+        snippet = Snippet(Type.NAME, words[1], line_number, parent)
+    elif words[0].startswith('constructor'):
+        snippet = Snippet(Type.CONSTRUCTOR, words[0], line_number, parent)
+    elif words[0] == 'static':
+        if words[1] in ('get', 'set'):
+            if words[2][0] == '#':
+                snippet = Snippet(
+                    Type.STATIC_PRIVATE_GETTER, code, line_number, parent)
+            else:
+                snippet = Snippet(
+                    Type.STATIC_PUBLIC_GETTER, code, line_number, parent)
+        elif nested_testcase_re.search(code):
+            snippet = Snippet(
+                Type.NESTED_TESTCASE, words[1], line_number, parent)
+        elif static_class_re.search(code):
+            if words[1][0] == '#':
+                snippet = Snippet(
+                    Type.STATIC_PRIVATE_CLASS, words[1], line_number, parent)
+            else:
+                snippet = Snippet(
+                    Type.STATIC_PUBLIC_CLASS, words[1], line_number, parent)
+        elif method_re.search(code):
+            if words[1][0] == '#':
+                snippet = Snippet(
+                    Type.STATIC_PRIVATE_METHOD, code, line_number, parent)
+            else:
+                snippet = Snippet(
+                    Type.STATIC_PUBLIC_METHOD, code, line_number, parent)
+        elif words[1][0] == '#':
+            snippet = Snippet(
+                Type.STATIC_PRIVATE_FIELD, code, line_number, parent)
+        else:
+            snippet = Snippet(
+                Type.STATIC_PUBLIC_FIELD, code, line_number, parent)
     else:
-      snippet = Snippet(Type.STATIC_PUBLIC_FIELD, code, line_number, parent)
-  else:
-    if words[0] in ('get', 'set'):
-      if words[1][0] == '#':
-        snippet = Snippet(Type.PRIVATE_GETTER, code, line_number, parent)
-      else:
-        snippet = Snippet(Type.PUBLIC_GETTER, code, line_number, parent)
-    elif method_re.search(code):
-      if words[0][0] == '#':
-        snippet = Snippet(Type.PRIVATE_METHOD, code, line_number, parent)
-      else:
-        snippet = Snippet(Type.PUBLIC_METHOD, code, line_number, parent)
-    elif words[0][0] == '#':
-      snippet = Snippet(Type.PRIVATE_FIELD, code, line_number, parent)
-    else:
-      if 'new Shortcut' in code:
-        snippet = Snippet(Type.PUBLIC_METHOD, code, line_number, parent)
-      else:
-        # For lack of a proper JS parser, making a guess that things
-        # looking like assigning a field is actually a local variable
-        # in a method.
-        likely_local_variable_assignment = indent > (parent.indent + 2)
-        if not likely_local_variable_assignment:
-          snippet = Snippet(Type.PUBLIC_FIELD, code, line_number, parent)
+        if words[0] in ('get', 'set'):
+            if words[1][0] == '#':
+                snippet = Snippet(
+                    Type.PRIVATE_GETTER, code, line_number, parent)
+            else:
+                snippet = Snippet(
+                    Type.PUBLIC_GETTER, code, line_number, parent)
+        elif method_re.search(code):
+            if words[0][0] == '#':
+                snippet = Snippet(
+                    Type.PRIVATE_METHOD, code, line_number, parent)
+            else:
+                snippet = Snippet(
+                    Type.PUBLIC_METHOD, code, line_number, parent)
+        elif words[0][0] == '#':
+            snippet = Snippet(Type.PRIVATE_FIELD, code, line_number, parent)
+        else:
+            if 'new Shortcut' in code:
+                snippet = Snippet(
+                    Type.PUBLIC_METHOD, code, line_number, parent)
+            else:
+                # For lack of a proper JS parser, making a guess that things
+                # looking like assigning a field is actually a local variable
+                # in a method.
+                likely_local_variable_assignment = indent > (
+                    parent.indent + 2)
+                if not likely_local_variable_assignment:
+                    snippet = Snippet(
+                        Type.PUBLIC_FIELD, code, line_number, parent)
 
-  return snippet
+    return snippet
 
 
 def process(filename):
-  """Lint the given filename."""
-  classes = list()
-  current = list()
-  nesting = [Nest(0, 0, '')]
-  in_class = False
+    """Lint the given filename."""
+    classes = list()
+    current = list()
+    nesting = [Nest(0, 0, '')]
+    in_class = False
 
-  for num, line in enumerate(open(filename, encoding='utf-8').readlines(),
-                             start=1):
-    words = line.split()
-    if line.startswith(' ') and words:
-      code = words.pop(0)
-      indent = line.index(code)
-      parent = nesting[-1]
+    for num, line in enumerate(open(filename, encoding='utf-8').readlines(),
+                               start=1):
+        words = line.split()
+        if line.startswith(' ') and words:
+            code = words.pop(0)
+            indent = line.index(code)
+            parent = nesting[-1]
 
-      if code in (
-          'const', 'if', 'await', 'return', 'for', 'while', 'function', 'let',
-          'throw', 'new', 'try'
-      ) or '.' in code or '`' in code or "'" in code or code.startswith(
-          '(') or code.startswith('super('):
-        if code == 'function' and (indent <= parent.indent
-                                   or not parent.indent):
-          in_class = False
-        continue
+            if code in (
+                    'const', 'if', 'await', 'return', 'for', 'while',
+                    'function', 'let', 'throw', 'new', 'try'
+            ) or '.' in code or '`' in code or "'" in code or code.startswith(
+                    '(') or code.startswith('super('):
+                if code == 'function' and (indent <= parent.indent
+                                           or not parent.indent):
+                    in_class = False
+                continue
 
-      if indent <= parent.indent:
-        nesting.pop()
-        parent = nesting[-1]
+            if indent <= parent.indent:
+                nesting.pop()
+                parent = nesting[-1]
 
-      if code.isalnum() or code[0] in ('#', '_') or '(' in code:
-        if 'class' in [code] + words:
-          in_class = True
-          nest = Nest(indent, num, words[0])
-          nesting.append(nest)
+            if code.isalnum() or code[0] in ('#', '_') or '(' in code:
+                if 'class' in [code] + words:
+                    in_class = True
+                    nest = Nest(indent, num, words[0])
+                    nesting.append(nest)
 
-        if code in ('class', 'static', 'get', 'set', 'async'):
-          code = f'{code} {words.pop(0)}'
-          if code in ('static {', 'async ()'):
-            continue
+                if code in ('class', 'static', 'get', 'set', 'async'):
+                    code = f'{code} {words.pop(0)}'
+                    if code in ('static {', 'async ()'):
+                        continue
 
-        if not in_class:
-          continue
+                if not in_class:
+                    continue
 
-        code += ' ' + ' '.join(words)
+                code += ' ' + ' '.join(words)
 
-        if skip_re.search(code):
-          # print('skipping', code)
-          continue
+                if skip_re.search(code):
+                    # print('skipping', code)
+                    continue
 
-        if indent < 8:
-          snippet = extract_snippet(code, num, parent, indent)
-          if snippet:
-            # New class
-            if snippet.type == Type.NAME and current:
-              if current[0].type == Type.NAME:
-                classes.append(current)
-              current = []
-            current.append(snippet)
+                if indent < 8:
+                    snippet = extract_snippet(code, num, parent, indent)
+                    if snippet:
+                        # New class
+                        if snippet.type == Type.NAME and current:
+                            if current[0].type == Type.NAME:
+                                classes.append(current)
+                            current = []
+                        current.append(snippet)
 
-  # Catch the last class being worked on
-  if current and current[0].type == Type.NAME:
-    classes.append(current)
+    # Catch the last class being worked on
+    if current and current[0].type == Type.NAME:
+        classes.append(current)
 
-  clean = True
-  for item in classes:
-    srt = tsort(item)
-    if srt != item:
-      clean = False
-      print(f'bad: {filename}: {item[0]}')
-      item = [str(x) for x in item]
-      srt = [str(x) for x in srt]
-      print('\n'.join(difflib.unified_diff(item, srt)))
-      print('\n\n')
+    clean = True
+    for item in classes:
+        srt = tsort(item)
+        if srt != item:
+            clean = False
+            print(f'bad: {filename}: {item[0]}')
+            item = [str(x) for x in item]
+            srt = [str(x) for x in srt]
+            print('\n'.join(difflib.unified_diff(item, srt)))
+            print('\n\n')
 
-  return clean
+    return clean
+
 
 def main():
-  """Main."""
-  clean = True
-  for filename in (glob.glob('**/*.js', recursive=True)):
-    clean &= process(filename)
+    """Main."""
+    clean = True
+    for filename in (glob.glob('**/*.js', recursive=True)):
+        clean &= process(filename)
 
-  return not clean
+    return not clean
+
 
 if __name__ == '__main__':
     sys.exit(main())
