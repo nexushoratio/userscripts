e92083cb6484d0c421f512c9d023b737f3513d78
Mike Castle <dalgoda@gmail.com>
2026-06-10 09:28:26 -0700

    Fine tune the Style-2 body selectors.
    At least the Search results page has a different number of `div`s present.
    
    ␄

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -3180,7 +3180,7 @@
       // Style 1
       'aside.scaffold-layout__aside',
       // Style 2
-      '#workspace > div > div > section + aside',
+      '#workspace > div div > section + aside',
     ].join(', ');
 
     static #icon =
@@ -3206,7 +3206,7 @@
 
     static #primaryContentSelector = [
       // Style 2
-      '#workspace > div > div > section',
+      '#workspace > div div > section',
     ].join(', ');
 
     static #primaryNavSelector = [
@@ -3223,7 +3223,7 @@
       // Style 1
       'aside.scaffold-layout__sidebar',
       // Style 2
-      '#workspace > div > div > aside:has(+ section)',
+      '#workspace > div div > aside:has(+ section)',
     ].join(', ');
 
     /**
