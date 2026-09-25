90fce75d321775235f81cf1d215a597a7a0ddc08
Mike Castle <dalgoda@gmail.com>
2026-09-25 11:04:50 -0700

    nav

--- linkedin-tool.user.js
+++ linkedin-tool.user.js
@@ -31,7 +31,7 @@
 // @require http://localhost:8000/userscripts/lib/userscript.js?0
 // @require http://localhost:8000/userscripts/lib/web.js?0
 // @require http://localhost:8000/userscripts/lib/widget.js?0
-// @require http://localhost:8000/userscripts/lib/nav.js?1314452916
+// @require http://localhost:8000/userscripts/lib/nav.js?2958037685
 // ==/UserScript==
 
 /**
