40939ae355b3a2f0375f743bcefe00668344c027
Mike Castle <dalgoda@gmail.com>
2026-09-25 11:53:05 -0700

    nav

--- demos.user.js
+++ demos.user.js
@@ -26,7 +26,7 @@
 // @require http://localhost:8000/userscripts/lib/userscript.js?0
 // @require http://localhost:8000/userscripts/lib/web.js?0
 // @require http://localhost:8000/userscripts/lib/widget.js?0
-// @require http://localhost:8000/userscripts/lib/nav.js?1237673202
+// @require http://localhost:8000/userscripts/lib/nav.js?3258076890
 // ==/UserScript==
 
 /**
